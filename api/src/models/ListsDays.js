c0a46f78e47d47f6fdbf1d9fb5dd8c5ecf545826
Lucas Ibar <106266661+lucasibar@users.noreply.github.com>
2023-10-11 15:05:06 -0300

    Update ListsDays.js

--- api/src/models/ListsDays.js
+++ api/src/models/ListsDays.js
@@ -1,1 +1,20 @@
+const { DataTypes } = require("sequelize");
 
+module.exports = (sequelize) => {
+  sequelize.define(
+    "ListDays",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      dayName: {
+        type: DataTypes.STRING
+      }
+    },
+    {
+      timestamps: false,
+    }
+  );
+};
