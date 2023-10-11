57c1fa543f769bb35a748222359d478ddadc2bb9
Lucas Ibar <106266661+lucasibar@users.noreply.github.com>
2023-10-11 12:44:56 -0300

    rutas del dia pero me surgieron un monton de dudas

--- api/src/routes/rutasPrincipales/daysRoutes.js
+++ api/src/routes/rutasPrincipales/daysRoutes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const { getDays } = require('./controllers/Day/getDays')
 const { getDayRecipes } = require('./controllers/Day/getDayRecipes')
+const { postNewDay } = require('./controllers/Day/postNewDay')
 
 
 const daysRoutes = Router();
@@ -14,6 +15,19 @@ daysRoutes.get('/:id', async (req, res)=>{
     try{res.status(200).json(await getDayRecipes(id))}
     catch(error){res.status(400).json({Error: error.message})} 
 })
+daysRoutes.post('/', async (req, res)=>{
+    try{
+        const newDay = req.body
+        res.status(200).json(await postNewDay(newDay))}
+    catch(error){res.status(400).json({Error: error.message})} 
+})
+daysRoutes.put('/', async (req, res)=>{
+    try{
+        const dayName = req.body
+        res.status(200).json(await changeName(dayName))}
+    catch(error){res.status(400).json({Error: error.message})} 
+})
+
 
 
 
