53da25e21481ac556f9d5e2b7b92e74fc1847c1b
Lucas Ibar <106266661+lucasibar@users.noreply.github.com>
2023-10-12 14:57:03 -0300

    Update daysRoutes.js

--- api/src/routes/rutasPrincipales/daysRoutes.js
+++ api/src/routes/rutasPrincipales/daysRoutes.js
@@ -10,24 +10,46 @@ daysRoutes.get('/', async (req, res)=>{
     try{res.status(200).json(await getDays())}
     catch(error){res.status(400).json({Error: error.message})} 
 })
-daysRoutes.get('/:id', async (req, res)=>{
-    const {id} = req.params
-    try{res.status(200).json(await getDayRecipes(id))}
+daysRoutes.get('/:idDay', async (req, res)=>{
+    const {idDay} = req.params
+    try{res.status(200).json(await getDayRecipes(idDay))}
     catch(error){res.status(400).json({Error: error.message})} 
 })
 daysRoutes.post('/', async (req, res)=>{
     try{
-        const newDay = req.body
-        res.status(200).json(await postNewDay(newDay))}
-    catch(error){res.status(400).json({Error: error.message})} 
+        // ejemplo= {idDay:null, meal:"almuerzo", ingredient: idIngredient, recipe: null((idRecipe)) , amount: 3 , portions: null}
+        const newMeal = req.body
+        res.status(200).json(await addMealToDay(idDay, newMeal))
+    }
+    catch(error){res.status(400).json({Error: error.message})
+    } 
 })
 daysRoutes.put('/', async (req, res)=>{
     try{
-        const dayName = req.body
-        res.status(200).json(await changeName(dayName))}
+        //{
+        //idDay:ejemplo,
+        //recipes:[{idRecipe: tanto, porcions:6}, {idRecipe: tanto2, porcions:6}],
+        // ingredients:[{idIngredient: tanto, amount:6}, {idIngredient: tanto2, amount:6}]
+        //}
+        
+        const changesOfAmounts = req.body
+        const {idDay} = req.params   
+        res.status(200).json(await changeAmount(idDaydayName))}
     catch(error){res.status(400).json({Error: error.message})} 
 })
-
+daysRoutes.delet('/', async (req, res)=>{
+    try{
+        //{
+        // idDay: tanto ,
+        // idRecipe: null,
+        // idIngredient: tanto3
+        //}
+        
+        const changesOfAmounts = req.body
+        const {idDay} = req.params   
+        res.status(200).json(await changeAmount(idDaydayName))}
+    catch(error){res.status(400).json({Error: error.message})} 
+})
 
 
 
