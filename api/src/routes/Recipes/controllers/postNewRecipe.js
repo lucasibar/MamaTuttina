14fad547fddd3da553b8e7f577de65832b001545
Lucas Ibar <106266661+lucasibar@users.noreply.github.com>
2023-10-24 15:00:53 -0300

    Update postNewRecipe.js

--- api/src/routes/Recipes/controllers/postNewRecipe.js
+++ api/src/routes/Recipes/controllers/postNewRecipe.js
@@ -1,7 +1,30 @@
-const { Users, Days, Meals } = require("../../../db");
+const { Users, Days, Meals, Recipes, Ingredients } = require("../../../db");
 const {Op} = require('sequelize');
 
-const postNewRecipe = async function (recipe) {   
+const postNewRecipe = async function ({recipe, idUser}) {   
+  //ejemplo
+  //{name: "Arroz con Pollo", category: "pollo" recipe: [{ ingredient: idIngredient, amount: 3, unti:grs }, { ingredient: idIngredient, amount: 3, unti:grs } ]}
+  const user = Users.findOne({
+    where:{
+      id: idUser
+    }
+  })
+const recipeName = await Recipes.create({
+name: recipe.name,
+category: recipe.category
+})
+  
+  for (let i=0; i<recipe.length; i++){
+    const ingredient = recipe[i]
+    await RecipeIngredient.create({
+    RecipeId: recipeName.id,
+    IngredientId: ingredient.ingredient,
+    amount: ingredient.amount,
+    unit: ingredient.unit
+    }) 
+  }
+   
+  
   const newRecipe = await Days.create(recipe)
   
 
